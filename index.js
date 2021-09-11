cf1ed104c4038dc692aeff23bc4952617ded5275
Chirag Gupta <51075671+Hardiegogo@users.noreply.github.com>
2021-09-12 03:30:21 +0530

    Added html,js and css files

---
+++ index.js
@@ -0,0 +1,36 @@
+const initialPrice=document.querySelector('#initial-price');
+const currentPrice=document.querySelector('#current-price')
+const stockQuantity=document.querySelector('#stock-quantity')
+const checkBtn=document.querySelector('#check-btn')
+const outputArea=document.querySelector('#output-area')
+const imgLeft=document.querySelector('.img-left')
+const imgRight = document.querySelector(".img-right");
+
+function showProfitLoss(){
+    let startPrice= Number(initialPrice.value);
+    let current= Number(currentPrice.value);
+    let quantity=Number(stockQuantity.value);
+
+    if(startPrice>current){
+        let loss=(startPrice-current)*quantity;
+        let lossPercent=(loss/startPrice)*100;
+        imgLeft.style.display = "none";
+        imgRight.style.display = "none";
+
+        outputArea.innerHTML=`Your loss is ${loss} and the loss percentage is ${lossPercent}!`
+        
+    }else if(startPrice<current){
+        let profit=(current-startPrice)*quantity;
+        
+        let profitPercent=(profit/startPrice)*100;
+        imgLeft.style.display='block';
+        imgRight.style.display = "block";
+        outputArea.innerHTML = `Your profit is ${profit} and the profit percentage is ${profitPercent}!`;
+    }else{
+        outputArea.innerHTML='No gain, No pain'
+    }
+}
+
+checkBtn.addEventListener('click',()=>{
+    showProfitLoss();
+})
